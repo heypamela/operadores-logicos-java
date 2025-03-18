072943cf43a29b01040a007a883788511e64ea98
Pamela <pamsilva.trabalho@hotmail.com>
2025-03-18 14:06:54 -0300

    Exercise 4 done

--- src/exercicios_operadores_variaveis/Exercicio4CalculeDiferenca.java
+++ src/exercicios_operadores_variaveis/Exercicio4CalculeDiferenca.java
@@ -1,5 +1,30 @@
 package exercicios_operadores_variaveis;
 
-public class Exercicio4CalculeDiferenca {
+import java.util.Scanner;
 
+public class Exercicio4CalculeDiferenca {
+    public static void main(String[] args) {
+        Scanner lerValor = new Scanner(System.in);
+
+        float numero1, numero2, numero3, numero4, diferenca;
+
+        System.out.println("Digite numero 1: ");
+        numero1 = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite numero 2: ");
+        numero2 = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite numero 3: ");
+        numero3 = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite numero 4: ");
+        numero4 = lerValor.nextFloat();
+        
+        diferenca = (numero1 * numero2) - (numero3 * numero4) ;
+
+        System.out.printf("Diferença: %.1f", diferenca);
+    }
 }
