1c7120708f58aae482c40efd448db26e7a7f3da0
Pamela <pamsilva.trabalho@hotmail.com>
2025-03-18 14:01:54 -0300

    Exercise 3 done

--- src/exercicios_operadores_variaveis/Exercicio3SalarioBruto.java
+++ src/exercicios_operadores_variaveis/Exercicio3SalarioBruto.java
@@ -1,5 +1,29 @@
 package exercicios_operadores_variaveis;
 
-public class Exercicio3SalarioBruto {
+import java.util.Scanner;
 
+public class Exercicio3SalarioBruto {
+    public static void main(String[] args) {
+        Scanner lerValor = new Scanner(System.in);
+        float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+
+        System.out.println("Digite o salário bruto: ");
+        salarioBruto = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite o adicional noturno: ");
+        adicionalNoturno = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite as horas extras: ");
+        horasExtras = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite os descontos: ");
+        descontos = lerValor.nextFloat();
+
+        salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+
+        System.out.printf("Salário Líquido: %.2f", salarioLiquido);
+    }
 }
