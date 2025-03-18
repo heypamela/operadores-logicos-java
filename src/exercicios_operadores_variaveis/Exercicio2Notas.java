22889aa246b9c4f51c8d7554baa5ed7ffef745ff
Pamela <pamsilva.trabalho@hotmail.com>
2025-03-18 13:53:37 -0300

    Deleted folders and exercise 2 done

--- src/exercicios_operadores_variaveis/Exercicio2Notas.java
+++ src/exercicios_operadores_variaveis/Exercicio2Notas.java
@@ -1,5 +1,30 @@
 package exercicios_operadores_variaveis;
 
-public class Exercicio2Notas {
+import java.util.Scanner;
 
+public class Exercicio2Notas {
+    public static void main(String[] args) {
+        Scanner lerValor = new Scanner(System.in);
+
+        float nota1, nota2, nota3, nota4, mediaFinal;
+
+        System.out.println("Digite nota 1: ");
+        nota1 = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite nota 2: ");
+        nota2 = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite nota 3: ");
+        nota3 = lerValor.nextFloat();
+
+        lerValor.skip("\\R?");
+        System.out.println("Digite nota 4: ");
+        nota4 = lerValor.nextFloat();
+        
+        mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.printf("Média final: %.1f", mediaFinal);
+    }
 }
